6f90b09318bdad84a0552c2a3dd51658e7aff3da
AndjelinaMaksimovic <maksimovicandjelina@gmail.com>
2025-04-04 22:24:14 +0200

    Remove build folder from version control

--- build/idea-sandbox/system/tmp/intellij-ssh-askpass-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/Users/Andjelina.Maksimovic/.gradle/caches/modules-2/files-2.1/com.jetbrains/jbre/jbr_jcef-17.0.12-osx-aarch64-b1000.54/extracted/jbr_jcef-17.0.12-osx-aarch64-b1000.54/Contents/Home/bin/java" -cp "/Users/Andjelina.Maksimovic/.gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIC/2023.2.8/7c86eea75125bc2c7a9bb0de5ef835a12fc4cd91/ideaIC-2023.2.8/lib/externalProcess-rt.jar" externalApp.nativessh.NativeSshAskPassApp "$@"
